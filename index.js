cee0db87745dee184721b7caf20a0dc8c185770a
horusjavier <kornjaviersanchezb@gmail.com>
2023-06-06 20:35:33 -0500

    landing  page

---
+++ index.js
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>KLAMZ - Alivio para dolores de cabeza y migrañas</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <style>
+    /* Estilos adicionales personalizados */
+    /* Agrega aquí tus estilos personalizados si los necesitas */
+  </style>
+</head>
+<body>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">KLAMZ</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#features">Características</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#order">Ordenar</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+
+  <section id="hero" class="py-5 text-center">
+    <div class="container">
+      <h1 class="display-4">¡Presentamos KLAMZ: la solución para aliviar dolores de cabeza y migrañas de forma natural!</h1>
+      <p class="lead">¿Estás cansado de lidiar con dolores de cabeza y migrañas debilitantes? ¡No busques más! KLAMZ es el producto revolucionario que cambiará tu vida. Nuestra envoltura para la cabeza utiliza una combinación de terapia de frío y compresión suave para brindarte alivio instantáneo y duradero.</p>
+    </div>
+  </section>
+
+  <section id="features" class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <h2>Características destacadas de KLAMZ:</h2>
+          <ul>
+            <li>Alivio natural: Olvídate de los analgésicos y tratamientos invasivos. KLAMZ utiliza métodos naturales para aliviar el dolor de cabeza y la migraña. La terapia de frío ayuda a reducir la inflamación y la compresión suave brinda un alivio reconfortante.</li>
+            <li>Diseño ergonómico: Nuestra envoltura para la cabeza KLAMZ ha sido diseñada pensando en tu comodidad. Se adapta perfectamente a la forma de tu cabeza y cuenta con un cierre ajustable para garantizar un ajuste seguro y cómodo.</li>
+            <li>Fácil de usar: KLAMZ es extremadamente fácil de usar. Simplemente coloca la envoltura en el congelador durante unos minutos, ajústala a tu cabeza y disfruta del alivio refrescante. ¡No se requieren complicados procedimientos ni montajes!</li>
+            <li>Versatilidad: KLAMZ no solo es efectivo para aliviar dolores de cabeza y migrañas, sino que también puede ayudar en el tratamiento de otras condiciones, como fiebre, tensión muscular y estrés. Es un producto versátil y multiusos que se adapta a tus necesidades.</li>
+            <li>Portátil y duradero: KLAMZ es compacto y fácil de transportar, lo que te permite llevarlo contigo a donde quiera que vayas. Además, está construido con materiales de alta calidad que garantizan su durabilidad a largo plazo.</li>
+          </ul>
+        </div>
+        <div class="col-md-6">
+          <img src="https://m.media-amazon.com/images/I/51bZ0wAFADL._AC_SX679_.jpg" alt="KLAMZ" class="img-fluid">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="order" class="py-5 text-center">
+    <div class="container">
+      <h2>¡Ordena tu KLAMZ hoy mismo y experimenta el alivio instantáneo que mereces!</h2>
+      <p>Haz clic en el enlace a continuación para comprar tu KLAMZ ahora:</p>
+      <a href="https://www.amazon.com/Klamz-Head-Wrap-Migraine-Relief/dp/B0BT15D37Y/ref=sr_1_1?keywords=KLAMZ&qid=1686094614&sr=8-1" class="btn btn-primary">Comprar en Amazon</a>
+    </div>
+  </section>
+
+  <footer class="py-3 text-center">
+    <div class="container">
+      <p>© 2023 DesarrolloExperto. Todos los derechos reservados.</p>
+    </div>
+  </footer>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</body>
+</html>
